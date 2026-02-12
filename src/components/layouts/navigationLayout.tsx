52f3edb888c571884af6a23bf6e493ce390ef612
SUCCESS <rabbiincode@gmail.com>
2026-02-12 13:27:32 +0100

    Functional Header and Sidebar UI

---
+++ src/components/layouts/navigationLayout.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useState } from "react";
+import { Header } from "./Header";
+import { Sidebar } from "./Sidebar";
+import { Box } from "@chakra-ui/react";
+
+export default function NavigationLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <>
+      <Sidebar isOpen={sidebarOpen} />
+
+      <Header
+        sidebarOpen={sidebarOpen}
+        toggleSidebar={() => setSidebarOpen(!sidebarOpen)}
+      />
+
+      <Box
+        ml={sidebarOpen ? "260px" : "0"}
+        pt="4rem"
+        transition="all 0.2s ease"
+      >
+        {children}
+      </Box>
+    </>
+  );
+}
